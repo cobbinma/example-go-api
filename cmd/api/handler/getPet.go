b112ceff81516af96d4ce8338269e8d5ece717d2
matthewcobbing <matthewc@nandos.co.uk>
2020-04-05 22:50:24 +0100

    add working tests for get pet

--- cmd/api/handler/getPet.go
+++ cmd/api/handler/getPet.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"database/sql"
 	"errors"
+	"fmt"
 	"github.com/cobbinma/example-go-api/pkg/models"
 	"github.com/labstack/echo/v4"
 	"github.com/sirupsen/logrus"
@@ -19,18 +20,18 @@ func (h *handler) GetPet(c echo.Context) error {
 		return c.JSON(http.StatusBadRequest, models.NewErrorResponse(pErr))
 	}
 
-	pets, pErr := h.repository.GetPet(ctx, id)
+	pet, pErr := h.repository.GetPet(ctx, id)
 	if pErr != nil {
 		if errors.Is(pErr, sql.ErrNoRows) {
 			pErr.Wrap("could not find pet in repository")
 			logrus.Info(pErr)
-			return c.JSON(http.StatusBadRequest, models.NewErrorResponse(pErr))
+			return c.JSON(http.StatusNotFound, models.NewErrorResponse(pErr))
 		}
 		pErr.Wrap("error getting pet")
 		logrus.Error(pErr)
 		return c.JSON(http.StatusInternalServerError, models.NewErrorResponse(pErr))
 	}
-	return c.JSON(http.StatusOK, pets)
+	return c.JSON(http.StatusOK, pet)
 }
 
 func getID(c echo.Context) (int, models.PetError) {
@@ -38,7 +39,7 @@ func getID(c echo.Context) (int, models.PetError) {
 	id, err := strconv.Atoi(idStr)
 	if err != nil {
 		pErr := models.NewPetError(err, "could not parse pet id", 0)
-		pErr.Wrap("could not parse pet id")
+		pErr.Wrap(fmt.Sprintf("could not parse pet id '%s'", idStr))
 		return 0, pErr
 	}
 
