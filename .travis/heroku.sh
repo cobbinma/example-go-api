4bd66730ffaf8e3cfbee7d7a05342179c1f4712a
matthewcobbing <matthewc@nandos.co.uk>
2020-04-25 10:08:05 +0100

    debug

--- .travis/heroku.sh
+++ .travis/heroku.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 heroku container:login
+heroku help container
 heroku container:push web --app $HEROKU_APP_NAME
 heroku container:release web --app $HEROKU_APP_NAME
